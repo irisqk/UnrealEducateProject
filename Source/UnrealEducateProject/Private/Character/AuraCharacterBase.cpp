725630831a8f131cb0479a0fd9118e76c646b963
irisqk <2782607284@qq,com>
2024-05-20 15:55:48 +0800

    add the public and private folder

---
+++ Source/UnrealEducateProject/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,22 @@
+// it just a learning project copy by GAS write by chuqingkun,if you like it you also can study it
+
+
+#include "AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/UnrealEducateProject/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,29 @@
+// it just a learning project copy by GAS write by chuqingkun,if you like it you also can study it
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class UNREALEDUCATEPROJECT_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	AAuraCharacterBase();
+
+protected:
+
+	virtual void BeginPlay() override;
+//do not need to tick event and input event,it just a character base class ,that one will implementation in player character class and NPC character class
+// public:	
+// 	// Called every frame
+// 	virtual void Tick(float DeltaTime) override;
+//
+// 	// Called to bind functionality to input
+// 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
