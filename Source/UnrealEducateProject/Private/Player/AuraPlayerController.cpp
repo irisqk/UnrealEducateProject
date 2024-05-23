59a46e2614a0da6771162d926ad039bc047c37a2
irisqk <2782607284@qq,com>
2024-05-23 23:51:50 +0800

    add IA_Move and IMC_AuraContext and create the PlayerController class

---
+++ Source/UnrealEducateProject/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// it just a learning project copy by GAS write by chuqingkun,if you like it you also can study it
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/UnrealEducateProject/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// it just a learning project copy by GAS write by chuqingkun,if you like it you also can study it
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALEDUCATEPROJECT_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
