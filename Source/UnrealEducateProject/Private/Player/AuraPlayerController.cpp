e9fd70e20b7d169e78acc5135b23e251262ec1e4
irisqk <2782607284@qq,com>
2024-05-29 23:23:59 +0800

    Aura Player Controller

--- Source/UnrealEducateProject/Private/Player/AuraPlayerController.cpp
+++ Source/UnrealEducateProject/Private/Player/AuraPlayerController.cpp
@@ -3,3 +3,29 @@
 
 #include "Player/AuraPlayerController.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(
+		GetLocalPlayer());
+	check(SubSystem);
+	SubSystem->AddMappingContext(AuraContext, 0);
+	//show mouse
+	bShowMouseCursor = true;
+	//set the mouse cursor to default
+	DefaultMouseCursor = EMouseCursor::Default;
+	//允许同时响应游戏和界面ui
+	FInputModeGameAndUI InputModeData;
+	//设置鼠标锁定模式为不锁定，（允许鼠标移出游戏窗口，例如lol是锁定）
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	//当光标的输入被被捕获时，不隐藏光标，（例如ff14控制鼠标控制人物方向，鼠标会消失，配置为false后则不会消失）
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
--- Source/UnrealEducateProject/Public/Player/AuraPlayerController.h
+++ Source/UnrealEducateProject/Public/Player/AuraPlayerController.h
@@ -4,8 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
 
+
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +17,14 @@ UCLASS()
 class UNREALEDUCATEPROJECT_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere,Category="Input")	
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
